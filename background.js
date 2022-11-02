db2984f5b10352227373be96ff1c12cb2ccbcba3
baspi <bartekspitza@hotmail.com>
2022-11-02 22:08:59 +0100

    added popup

---
+++ background.js
@@ -0,0 +1,6 @@
+const extensions = 'https://developer.chrome.com/docs/extensions'
+const webstore = 'https://developer.chrome.com/docs/webstore'
+
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log('zxcv')
+});
